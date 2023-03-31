4fe34b4d85cc5947074ff0b0eef53d6f620067da
Matt P <54957569+mtp354@users.noreply.github.com>
2023-03-31 15:12:48 -0400

    Added Percentage Contribution Calculations

--- APC Template.R
+++ APC Template.R
@@ -12,7 +12,7 @@ cat("\014") # Clearing Console
 
 # Importing SEER Data, starting with Incidence by Year & Age
 # df <- read.csv(file.choose())  # Use this line to navigate to file path if needed
-df <- read.csv("C:\\Users\\mattp\\OneDrive\\Documents\\Columbia Stuff\\APC Analysis\\S8 GC Incidence Age Year.csv")
+df <- read.csv("W:\\Matt P\\Projects\\202201 CISNET\\Uterine\\APC\\S8 GC Incidence Age Year.csv")
 
 # Cleaning SEER Data, getting year and age as integer values only
 df <- df %>% filter(!(Year %in% c("1975-2019", "Statistic could not be calculated.", "Rates are per 100,000 and age-adjusted to the 2000 US Std Population (single ages to 84 - Census P25-1130) standard.")))
@@ -36,17 +36,18 @@ df <- as.matrix(df)  # Convert to matrix
 df <- discretize(df)  # Discretizing
 
 # Mutual information matrix:
-info_mat <- round(mutinformation(df), 7)
+info <- round(mutinformation(df), 7)
+
+# Converting to % contribution
+info <- info[4,1:3]/info[4,4]
+info <- 100*info/sum(info)
+
+# Converting to Dataframe for export
+info <- data.frame(info)
+names(info) <- c("Variable")
 
 # For exporting to excel via clipboard:
-write.table(info_mat, file = "clipboard", sep = "\t", row.names = F, col.names = T)
-
-
-
-
-
-
-
+write.table(info, file = "clipboard", sep = "\t", row.names = T, col.names = T)
 
 
 
